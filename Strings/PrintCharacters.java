74e6697a6e386d6b7afb04f018c03a7aab13e50b
kavindu subhash <70571627+kavindusubhash28@users.noreply.github.com>
2025-07-17 10:24:27 +0530

    error resolved

---
+++ Strings/PrintCharacters.java
@@ -0,0 +1,8 @@
+public class PrintCharacters {
+    public static void main (String[] args) {
+        String str = "JAVA";
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+    }
+}

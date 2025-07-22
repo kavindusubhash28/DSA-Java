43b86df65299a8c3e1871bcd3dcd50c6b2f33161
kavindu subhash <70571627+kavindusubhash28@users.noreply.github.com>
2025-07-22 22:04:56 +0530

    updated

--- ArrayAverage.java
+++
@@ -1,9 +0,0 @@
-public class ArrayAverage {
-    public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40};
-        int sum = 0;
-        for (int num : arr) sum += num;
-        double avg = (double) sum / arr.length;
-        System.out.println("Average: " + avg); 
-    }
-}
